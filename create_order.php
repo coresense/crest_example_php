bf6387989358e09668f8770143a95be9f4a24d62
Alex Howansky <ahowansky@coresense.com>
2018-04-26 11:47:15 -0400

    Added create order example.

---
+++ create_order.php
@@ -0,0 +1,74 @@
+<?php
+
+// This creates a new order.
+
+$apiUrl = 'https://api-<your domain>.coresense.com/v1/customer';
+$userId = '<your user id>';
+$apiKey = '<your api key>';
+
+require_once 'functions.php';
+require_once 'vendor/autoload.php';
+
+$data = [
+
+	// This is the customer's ID. If this order is for a new customer, the
+	// customer record must first be created via the POST /v1/customer endpoint.
+	'customer_id' => 12345,
+
+	// The ID of the contact that the order will be billed to. If omitted,
+	// the customer's default billing contact will be used.
+	'billing_contact_id' => 234,
+
+	// The ID of the sales channel to attribute to order to. These can be
+	// enumerated via the GET /v1/channel endpoint.
+	'channel_id' => 1,
+
+	// An array of items to place on the order.
+	'items' => [
+		[
+
+			// The ID of the product to order. These can be enumerated via
+			// the GET /v1/product endpoint.
+			'product_id' => 123,
+
+			// The quantity to order. If omitted, defaults to 1.
+			'quantity' => 1,
+
+			// The ID of the contact that this item will be shipped to. If
+			// omitted, the customer's default shipping contact will be used.
+			'shipping_contact_id' => 789,
+
+			// The ID of the shipping method that this item will use. These
+			// can be enumerated via the GET /v1/shippingMethod endpoint.
+			'shipping_method_id' => 7,
+
+			// If provided, source the items from this warehouse. These can be
+			// enumerated via the GET /v1/warehouse endpoint. If this value is
+			// not provided, your existing sourcing rules will be used.
+			'warehouse_id' => 2,
+
+		],
+		[
+			'product_id' => 456,
+			'quantity' => 2,
+			'warehouse_id' => 3,
+		],
+	],
+];
+
+$client = new \GuzzleHttp\Client();
+try {
+	$res = $client->request('POST', $apiUrl, [
+		'headers' => [
+			'X-Auth-Token' => token($userId, $apiKey),
+		],
+		'json' => $data,
+	]);
+	print_r(json_decode($res->getBody(), true));
+} catch (\GuzzleHttp\Exception\ClientException $e) {
+	echo "request completed but response contained an error\n";
+	echo $e->getMessage();
+} catch (\GuzzleHttp\Exception\GuzzleException $e) {
+	echo "request failed\n";
+	echo $e->getMessage();
+}
